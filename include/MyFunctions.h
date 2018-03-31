a0ce0dc5db45c5cb21dcc713e2d7251c0b000538
Drake Jakobsons <37964020+DrakeJak@users.noreply.github.com>
2018-03-31 13:57:36 -0700

    Create MyFunctions.h

---
+++ include/MyFunctions.h
@@ -0,0 +1,10 @@
+#ifndef _MY_FUNCTIONS_
+#define _MY_FUNCTIONS_
+
+
+int hashFunction (size_t size, char* key);
+void printFunction(HTable* hashTable);
+void removeWord(void* data);
+void printWord(void* data);
+
+#endif
